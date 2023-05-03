a65030548accdf21a2f8186b6eb0328f61bf5c35
JinHyeok <jinhyuk@unist.ac.kr>
2023-05-03 04:18:21 +0000

    .

--- util/data.py
+++ util/data.py
@@ -19,23 +19,28 @@ class PairBreakingBadDataset(Dataset):
     
     
     def __len__(self):
-        return sum(self.num_pair_list)
+        return int(sum(self.num_pair_list))
     
     def _get_part_idx(self, object_idx, idx):
         leng = self.leng_list[object_idx]
-        ref_idx = idx
-        src_idx = 0
-        for i in range(leng, 0, -1):
-            if ref_idx < i:
-                break
-            else:
-                src_idx += 1
-                ref_idx -= i
+        count = 0
+        for i in range(leng):
+            for j in range(i + 1, leng):
+                if count == idx:
+                    return i, j
+                count += 1
             
-        return src_idx, ref_idx
+    
+    def _get_part_indices(n, index):
+        count = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if count == index:
+                    return i, j
+                count += 1
     
     def __getitem__(self, index):
-        index /= len(self)
+        # index = int(index) % int(len(self))
         
         object_idx = 0
         for n in self.num_pair_list:
@@ -46,6 +51,7 @@ class PairBreakingBadDataset(Dataset):
                 index -= n
         object = self.dataset[object_idx]
         src_idx, ref_idx = self._get_part_idx(object_idx, index)
+        import jhutil;jhutil.jhprint(3333, src_idx, ref_idx)
         src = object['breaking_pcs'][src_idx]
         ref =  object['breaking_pcs'][ref_idx]
         out = {
@@ -64,5 +70,9 @@ if __name__ == "__main__":
     torch.multiprocessing.set_start_method('spawn')
     cfg = jhutil.load_yaml("yamls/data_example.yaml")
     train_data, val_data = build_geometry_dataset(cfg)
+    import jhutil;jhutil.jhprint(1111, len(train_data))
+    import jhutil;jhutil.jhprint(0000, train_data[0])
     train_data = PairBreakingBadDataset(train_data)
-    import jhutil;jhutil.jhprint(1111, )
+    import jhutil;jhutil.jhprint(2222, len(train_data))
+    for data in train_data:
+        import jhutil;jhutil.jhprint(1111, data)
