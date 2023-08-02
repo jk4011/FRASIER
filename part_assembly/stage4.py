687d8edc7784ada4d89006374a00784b4b61d472
Jinhyeok <jinhyuk@unist.ac.kr>
2023-08-02 20:47:11 +0900

    merge_state : str -> frozenset

--- part_assembly/stage4.py
+++ part_assembly/stage4.py
@@ -26,27 +26,21 @@ logger = create_logger()
 
 
 class Fracture:
-    def __init__(self, pcd, merge_state, n_removed, n_last_removed, T_dic=None):
+    def __init__(self, pcd, merge_state:frozenset, n_removed:int, n_last_removed:int, T_dic=None):
+        assert isinstance(merge_state, frozenset)
         self.pcd = pcd
         self.merge_state = merge_state
         self.n_removed = n_removed
         self.n_last_removed = n_last_removed
 
         if T_dic is None:
-            assert isinstance(merge_state, int)
             T_dic = {merge_state: torch.eye(4)}
         self.T_dic = T_dic
         # TODO: bundle adjustment를 위해 original pcd도 저장해 두기
 
     @property
     def num_pcd(self):
-        def count(merge_state):
-            if isinstance(merge_state, int):
-                return 1
-            else:
-                return count(merge_state[0]) + count(merge_state[1])
-
-        return count(self.merge_state)
+        return len(self.merge_state)
 
     def merge(self, other, T):
         """merge two fracs
@@ -62,12 +56,8 @@ class Fracture:
         pcd_transformed = matrix_transform(T, other.pcd)
         new_pcd, n_last_removed = pointcloud_xor(self.pcd, pcd_transformed)
         n_removed = self.n_removed + other.n_removed + n_last_removed
-
-        # since merging is commutitive, fix order.
-        if self < other:
-            merge_state = [self.merge_state, other.merge_state]
-        else:
-            merge_state = [other.merge_state, self.merge_state]
+        
+        merge_state = self.merge_state.union(other.merge_state)
 
         other_T_dic = {merge_state: T @ prev_T for merge_state, prev_T in other.T_dic.items()}
         T_dict = {**self.T_dic, **other_T_dic}
@@ -93,7 +83,7 @@ class Fracture:
 class FractureSet:
     def __init__(self, pcd_list, use_icp=True):
         self.n_origin_pcd = sum([pcd.shape[0] for pcd in pcd_list])
-        self.fracs : list[Fracture] = [Fracture(pcd, merge_state=i, n_removed=0, n_last_removed=0) for i, pcd in enumerate(pcd_list)]
+        self.fracs : list[Fracture] = [Fracture(pcd, merge_state=frozenset([i]), n_removed=0, n_last_removed=0) for i, pcd in enumerate(pcd_list)]
         self.k = 3
         self.use_icp = use_icp
         self.use_similarity = False
@@ -145,11 +135,11 @@ class FractureSet:
     def search_one_step(self):
         if self.use_similarity:
             pairs = top_k_indices(self.similarity, self.k)
-        elif len(self.fracs) < 4:
+        elif len(self.fracs) < 3:
             pairs = [(i, j) for i in range(len(self.fracs)) for j in range(i+1, len(self.fracs))]
         else:
-            top4_idx = np.argsort([frac.pcd.shape[0] for frac in self.fracs])[-4:]
-            pairs = [(top4_idx[i], top4_idx[j]) for i in range(4) for j in range(i+1, 4)]
+            top3_idx = np.argsort([frac.pcd.shape[0] for frac in self.fracs])[-3:]
+            pairs = [(top3_idx[i], top3_idx[j]) for i in range(3) for j in range(i+1, 3)]
 
         new_frac_set_lst = []
         for i, j in pairs:
@@ -221,10 +211,23 @@ def is_item_in_priority_queue(pq, frac_set):
 def pointcloud_xor(src: torch.Tensor, ref: torch.Tensor, threshold=0.01):
     n_origin = src.shape[0] + ref.shape[0]
 
-    dist_src, _ = knn(src, ref)
-    dist_ref, _ = knn(ref, src)
-    src = src[dist_src > threshold]
-    ref = ref[dist_ref > threshold]
+    src_coarse = src.clone()
+    ref_coarse = ref.clone()
+    src_threshold = ref_threshold = threshold
+    
+    while len(src_coarse) > 3e4:
+        src_coarse = pcd_subsample(src_coarse)
+        ref_threshold *= 1.414
+        
+    while len(ref_coarse) > 3e4:
+        ref_coarse = pcd_subsample(ref_coarse)
+        src_threshold *= 1.414
+    
+    dist_src, _ = knn(src, ref_coarse)
+    dist_ref, _ = knn(ref, src_coarse)
+    
+    src = src[dist_src > src_threshold]
+    ref = ref[dist_ref > ref_threshold]
 
     n_after_xor = src.shape[0] + ref.shape[0]
     n_removed = n_origin - n_after_xor
@@ -251,64 +254,3 @@ def top_k_indices(matrix, k, only_triu=True):
     indices = torch.stack((row_indices, column_indices), dim=1).tolist()
 
     return indices
-
-
-def reproduce(pcd_list, merge_state):
-    # TODO: class frac_set로 바꾸기
-    left, right = merge_state
-
-    if not isinstance(left, int):
-        pcd1, n_removed1 = reproduce(pcd_list, left)
-    else:
-        pcd1 = pcd_list[left]
-        n_removed1 = 0
-
-    if not isinstance(right, int):
-        pcd2, n_removed2 = reproduce(pcd_list, right)
-    else:
-        pcd2 = pcd_list[right]
-        n_removed2 = 0
-
-    T = geo_transformer(pcd1, pcd2)
-    pcd2_transformed = matrix_transform(T, pcd2)
-
-    pcd_xored, n_removed = pointcloud_xor(pcd1, pcd2_transformed)
-    n_removed += n_removed1 + n_removed2
-    return pcd_xored, n_removed
-
-
-def test_reproduce(n_iter=10, n_obj_threshold=5):
-    cfg = load_yaml("/data/wlsgur4011/part_assembly/yamls/data_example.yaml")
-    train_loader, val_loader = build_geometry_dataloader(cfg, use_saved=True)
-
-    i = 0
-    for data in val_loader:
-        pcd_list = data["broken_pcs"]
-        if len(pcd_list) > n_obj_threshold:
-            continue
-        if i == n_iter:
-            break
-        i += 1
-
-        # TODO: point cloud는 gpu memory에서만 다루기
-        # pcd_list = to_cuda(pcd_list)
-
-        result = FractureSet(pcd_list).search()
-        final_frac = result.fracs[0]
-
-        import jhutil; jhutil.jhprint(1111, final_frac.merge_state)
-        import jhutil; jhutil.jhprint(2222, final_frac.T_dic)
-        import jhutil; jhutil.jhprint(5555, result.n_removed)
-
-        n_removed = result.n_removed
-        pcd_xored_re, n_removed_re = reproduce(pcd_list, final_frac.merge_state)
-        # assert torch.sum(pcd_xored - pcd_xored_re) < 1e-6, f"{torch.sum(pcd_xored - pcd_xored_re)}"
-        if n_removed != n_removed_re:
-            import jhutil; jhutil.jhprint(0000, f"{n_removed} is differs from {n_removed_re}")
-
-    import jhutil; jhutil.jhprint(0000, "test_reproduce done")
-
-
-def visualize_result():
-    # TODO: result를 visulaize해서 사진으로 저장해 두기
-    pass
